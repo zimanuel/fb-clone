f7438de0cdb90aa7d428caec4cb9e20b020220f1
amaniCoding <amanuelfrm@gmail.com>
2025-12-25 15:25:46 +0300

    new commit

--- app/components/home/post/common/content.tsx
+++ app/components/home/post/common/content.tsx
@@ -2,10 +2,13 @@ export default function PostContent() {
   return (
     <div className="px-3">
       <p className="font-mono">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit
-        debitis assumenda perferendis, earum consequuntur voluptatibus delectus
-        totam quasi ipsam ipsa temporibus porro in magni fugiat illo culpa harum
-        consequatur impedit?
+        The BBC witnessed dramatic scenes on a beach in of Northern France this
+        morning when a boat was destroyed by police moments before it set off
+        for the UK. All those onboard clambered to safety as the boat collapsed.
+        French police have strict rules that bar them from going into the sea,
+        but officers are allowed to intervene, if they believe lives are at
+        immediate risk. #France #Migrants #UK #MigrantBoat#Refugees
+        #NorthernFrance #News #BBCNews
       </p>
     </div>
   );
